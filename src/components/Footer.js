bf821c50ad7230959138e315f8a7b610f26dc884
Ms-Alex <ms.hernandeza1@gmail.com>
2018-12-10 19:06:05 -0500

    add header and footer components, and css

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            Love to code, but remember to take a break! <span role="img" aria-label="heart">❤️</span>
+        </div>
+    );
+}
+
+export default Footer;
